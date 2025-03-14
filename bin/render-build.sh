da6b9e98752c9f2d314d2445da99e73c2fa0fb2f
Kenny Mortimer <kennymortimer@KMortimer-Mac-01.local>
2025-03-14 11:50:54 -0600

    Fix PostgreSQL dependencies with apt buildpack

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,9 +2,6 @@
 # exit on error
 set -o errexit
 
-# Install PostgreSQL dependencies
-apt-get update -qq && apt-get install -y postgresql-client libpq-dev
-
 # Install dependencies
 bundle install
 
