17b45bfc5a38d59b740318ed9494bf9d3cc0894e
Kenny Mortimer <kennymortimer@KMortimer-Mac-01.local>
2025-03-14 10:43:42 -0600

    Simplify render-build.sh script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,15 +2,11 @@
 # exit on error
 set -o errexit
 
-# Configure bundler to use system libraries for pg gem
-bundle config --local build.pg --with-pg-config=/usr/bin/pg_config
-
 # Install dependencies
 bundle install
 
 # Asset compilation
 bundle exec rake assets:precompile
-bundle exec rake assets:clean
 
 # Database setup
 bundle exec rake db:migrate 
